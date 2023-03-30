7c458518c28521787c489eca35ff6b92ccec19ce
Christian Navoy <Christian.Navoy@PCCOMMS.CO.UK>
2023-03-30 11:40:34 +0100

    Initial commit

---
+++ M-CNC G Code WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace M_CNC_G_Code_WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
