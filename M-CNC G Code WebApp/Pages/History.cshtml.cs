47a2ec373440219cdaef71474e61d93379dc02a2
Christian Navoy <Christian.Navoy@PCCOMMS.CO.UK>
2023-03-30 13:05:56 +0100

    Added Table and start inputs

---
+++ M-CNC G Code WebApp/Pages/History.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace M_CNC_G_Code_WebApp.Pages
+{
+    public class HistoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
